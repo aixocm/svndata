27312fd764cdd42059e119c1d4a3dd4f4852a170
JJ <1755897532@qq.com>
2016-06-12 12:04:15 +0800

    day1

---
+++ Dday1/login.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env python 
+# coding:utf-8
+i=0
+while i < 3:
+	name = raw_input('please input your name:')
+	pwd = raw_input('please input your password:')
+	if name == 'JJ' and pwd == '123':
+		print 'success login!'
+		break	
+	i+=1
+else:
+		print 'you can not login again'
+		
+ 
